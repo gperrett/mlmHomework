3efaa25cd214374a5092cd6f8db6f265c5a9e5d5
George Perrett <gjperrett94@gmail.com>
2024-09-01 09:39:05 -0400

    hw5 and hw6

---
+++ R/hw5.R
@@ -0,0 +1,288 @@
+#' @title Load data for homework 2
+#' @param netid a character string representing your NYU netid. Make sure to only use your netID!!
+#' @return the `schools` data.frame
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # run code to load your unique dataset
+#' generate_data_hw5(netid = 'gp77') # put net id in quotes
+#'
+#'
+#' }
+#'
+generate_data_hw5 <- function(netid){
+  #draw <- readRDS('data/draw.rds')
+  id <- 1:200
+  names(id)[1:2] <- c('gp77', 'ms184')
+  names(id)[3:length(id)] <- 3:length(id)
+  names(draw) <- names(id)
+  Sys.setenv(seed = id[netid])
+  set.seed(as.numeric(Sys.getenv('seed')))
+  wagepan <<- draw5[[netid]]
+
+}
+
+
+
+#' @title submit answer to homework 5 question 1 part a
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 5a
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 4:
+#' answer_hw5_1a(answer = .2239)
+#'
+#' answer_hw5_1a(answer = .5483)
+#'
+#' }
+#'
+answer_hw5_1a <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 1 part a')
+  }
+
+  grade_env_hw5$answer_1a <- answer
+}
+
+#' @title submit answer to homework 5 question 1part b
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 5b
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 4:
+#' answer_hw5_1b(answer = .2239)
+#'
+#' answer_hw5_1b(answer = .5483)
+#'
+#' }
+#'
+answer_hw5_1b <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 1 part b')
+  }
+
+  grade_env_hw5$answer_1b <- answer
+}
+
+#' @title submit answer to homework 5 question 2
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 2
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 5:
+#' answer_hw5_2(answer = .2239)
+#'
+#' answer_hw5_2(answer = .5483)
+#'
+#' }
+#'
+answer_hw5_2 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 2')
+  }
+
+  grade_env_hw5$answer_2 <- answer
+}
+
+#' @title submit answer to homework 5 question 3
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 3
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 5:
+#' answer_hw5_3(answer = .2239)
+#'
+#' answer_hw5_3(answer = .5483)
+#'
+#' }
+#'
+answer_hw5_3 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 2')
+  }
+
+  grade_env_hw5$answer_3 <- answer
+}
+
+
+#' @title submit answer to homework 5 question 3
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 3
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 5:
+#' answer_hw5_3(answer = .2239)
+#'
+#' answer_hw5_3(answer = .5483)
+#'
+#' }
+#'
+answer_hw5_3 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 2')
+  }
+
+  grade_env_hw5$answer_3 <- answer
+}
+
+
+#' @title submit answer to homework 5 question 4
+#' @param answer a vector of length 19
+#' @return summary of your current answer for question 4
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 5:
+#'
+#' grid <- c(1, 2, 3, 4, 5) # should be length 19!
+#' answer_hw5_4(answer = grid)
+#'
+#'
+#' }
+#'
+answer_hw5_4 <- function(answer = NULL){
+
+  if(isFALSE(is.vector(answer))){
+    stop('your answer is not a vector. This question will only take a vector')
+  }
+
+  if(isFALSE(length(answer) == 19)){
+      warning('vector should be length 19 because you should have 19 different estimates')
+    }
+
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 4')
+  }
+
+  grade_env_hw5$answer_4 <- answer
+}
+
+
+
+#' @title submit answer to homework 5 question 5
+#' @param answer a vector of length 19
+#' @return summary of your current answer for question 5
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 5:
+#'
+#' grid <- c(1, 2, 3, 4, 5) # should be length 19!
+#' answer_hw5_5(answer = grid)
+#'
+#'
+#' }
+#'
+answer_hw5_5 <- function(answer = NULL){
+
+  if(isFALSE(is.vector(answer))){
+    stop('your answer is not a vector. This question will only take a vector')
+  }
+
+  if(isFALSE(length(answer) == 19)){
+    warning('vector should be length 19 because you should have 19 different estimates')
+  }
+
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 5')
+  }
+
+  grade_env_hw5$answer_5 <- answer
+}
+
+
+#' @title submit answer to homework 5 question 6
+#' @param answer a vector of length 19
+#' @return summary of your current answer for question 6
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 6:
+#'
+#' grid <- c(1, 2, 3, 4, 5) # should be length 19!
+#' answer_hw5_5(answer = grid)
+#'
+#'
+#' }
+#'
+answer_hw5_6 <- function(answer = NULL){
+
+  if(isFALSE(is.vector(answer))){
+    stop('your answer is not a vector. This question will only take a vector')
+  }
+
+  if(isFALSE(length(answer) == 19)){
+    warning('vector should be length 19 because you should have 19 different estimates')
+  }
+
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 5')
+  }
+
+  grade_env_hw5$answer_6 <- answer
+}
+
+
+#' @title submit answer to homework 5 question 7
+#' @param answer a logical indicating TRUE or FALSE
+#' @return summary of your current answer for question 7
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 2:
+#' answer_hw5_7(answer = TRUE) # to indicate true
+#'
+#' answer_hw5_7(answer = FALSE) # to indicate false
+#'
+#'
+#' }
+#
+answer_hw5_7 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 7')
+  }
+
+  if(is.character(answer)){
+    answer <- as.logical(answer)
+  }
+
+  grade_env_hw5$answer_7 <- answer
+}
+
+
+
+
---
+++ R/hw6.R
@@ -0,0 +1,199 @@
+#' @title Load data for homework 2
+#' @param netid a character string representing your NYU netid. Make sure to only use your netID!!
+#' @return the `schools` data.frame
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # run code to load your unique dataset
+#' generate_data_hw5(netid = 'gp77') # put net id in quotes
+#'
+#'
+#' }
+#'
+generate_data_hw6 <- function(netid){
+  #draw <- readRDS('data/draw.rds')
+  id <- 1:200
+  names(id)[1:2] <- c('gp77', 'ms184')
+  names(id)[3:length(id)] <- 3:length(id)
+  names(draw) <- names(id)
+  Sys.setenv(seed = id[netid])
+  set.seed(as.numeric(Sys.getenv('seed')))
+  dat <<- draw6[[netid]]
+
+}
+
+
+#' @title submit answer to homework 6 question 1
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 1
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 1:
+#' answer_hw6_1(answer = .2239)
+#'
+#' answer_hw6_1(answer = .5483)
+#'
+#' }
+#'
+answer_hw6_1 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 1')
+  }
+
+  grade_env_hw6$answer_1 <- answer
+}
+
+
+#' @title submit answer to homework 6 question 2
+#' @param answer a numeric answer. Do not worry about rounding but provide a minimum or 2 decimal places to all answers
+#' @return summary of your current answer for question 2
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 2:
+#' answer_hw6_2(answer = .2239)
+#'
+#' answer_hw6_2(answer = .5483)
+#'
+#' }
+#'
+answer_hw6_2 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 1')
+  }
+
+  grade_env_hw6$answer_2 <- answer
+}
+
+
+
+
+#' @title submit answer to homework 6 question 3 part a
+#' @param answer a vector
+#' @return summary of your current answer for question
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 3a:
+#'
+#' grid <- c(1, 2, 3, 4, 5) # should be length 19!
+#' answer_hw6_3a(answer = grid)
+#'
+#'
+#' }
+#'
+answer_hw6_3a <- function(answer = NULL){
+
+  if(isFALSE(is.vector(answer))){
+    stop('your answer is not a vector. This question will only take a vector')
+  }
+
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 4')
+  }
+
+  grade_env_hw6$answer_3a <- answer
+}
+
+
+#' @title submit answer to homework 6 question 3 part b
+#' @param answer a vector
+#' @return summary of your current answer for question
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 3b:
+#'
+#' grid <- c(1, 2, 3, 4, 5) # should be length 19!
+#' answer_hw6_3b(answer = grid)
+#'
+#'
+#' }
+#'
+answer_hw6_3b <- function(answer = NULL){
+
+  if(isFALSE(is.vector(answer))){
+    stop('your answer is not a vector. This question will only take a vector')
+  }
+
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 4')
+  }
+
+  grade_env_hw5$answer_3b <- answer
+}
+
+
+#' @title submit answer to homework 6 question 4
+#' @param answer a logical indicating TRUE or FALSE
+#' @return summary of your current answer for question 4
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 4:
+#' answer_hw6_4(answer = TRUE) # to indicate true
+#'
+#' answer_hw6_4(answer = FALSE) # to indicate false
+#'
+#'
+#' }
+#
+answer_hw6_4 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 7')
+  }
+
+  if(is.character(answer)){
+    answer <- as.logical(answer)
+  }
+
+  grade_env_hw6$answer_4 <- answer
+}
+
+
+#' @title submit answer to homework 6 question 6
+#' @param answer a logical indicating TRUE or FALSE
+#' @return summary of your current answer for question 6
+#' @export
+#'
+#' @examples
+#' \donttest{
+#'
+#' # format answer to question 6:
+#' answer_hw6_6(answer = TRUE) # to indicate true
+#'
+#' answer_hw6_6(answer = FALSE) # to indicate false
+#'
+#'
+#' }
+#
+answer_hw6_6 <- function(answer = NULL){
+  if(is.null(answer)){
+    answer <- NULL
+    warning('Currently, you have not answered question 7')
+  }
+
+  if(is.character(answer)){
+    answer <- as.logical(answer)
+  }
+
+  grade_env_hw6$answer_6 <- answer
+}
