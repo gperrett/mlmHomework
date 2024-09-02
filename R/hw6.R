a4d6e1b66d5a7526fdce162f3217ffb114b0fd9a
George Perrett <gjperrett94@gmail.com>
2024-09-02 12:39:48 -0400

    netId update

--- R/hw6.R
+++ R/hw6.R
@@ -15,8 +15,16 @@
 generate_data_hw6 <- function(netid){
   #draw <- readRDS('data/draw.rds')
   id <- 1:200
-  names(id)[1:2] <- c('gp77', 'ms184')
-  names(id)[3:length(id)] <- 3:length(id)
+  names(id)[1:58] <- c("gp77", "ms184", "zm2518", "msb7677", "uuc1", "sc5466", "ac11007",
+                       "sd5718", "yd2837", "emd14", "dld382", "sd5720", "kf1678", "mh7262",
+                       "jeh596", "xh2729", "yh2741", "jh172", "rbj2158", "yk3008", "sk10896",
+                       "yl7469", "alt10", "pl1900", "jl15426", "xl4820", "zl5566", "yl8844",
+                       "rl5309", "vl2414", "dl5334", "ebl9629", "sl10865", "yl11897",
+                       "ll1950", "cl6623", "sm11370", "ap8617", "bsp8894", "ap6355",
+                       "cp4101", "xq661", "zq2253", "kr3235", "jds866", "ss17504", "rlt9272",
+                       "at6099", "xt595", "vt2289", "cv985", "zw4183", "wy2391", "tz2569",
+                       "xz2661", "hz3533", "sz4548", "tz2683")
+  names(id)[59:length(id)] <- 59:length(id)
   names(draw6) <- names(id)
   Sys.setenv(seed = id[netid])
   set.seed(as.numeric(Sys.getenv('seed')))
