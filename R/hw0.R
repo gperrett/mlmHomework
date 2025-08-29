cbaa97dc7ff8b99ee00ba2a84b446f8a244b9233
marcascott <marcascott@users.noreply.github.com>
2025-08-28 21:55:57 -0400

    hw0 mainly
    skipping first 50

--- R/hw0.R
+++ R/hw0.R
@@ -2,11 +2,12 @@
 #' 
 
 nEnrolled <- 46
-nMax <- 150
-#add 50 in 2025:
-id <- 1:nMax + 50
+nMax <- 200
+id <- 1:nMax
+skip <- 50
+#start at 50 in 2025:
 names(id) <- as.character(1:length(id))
-names(id)[1:nEnrolled] <- c('gp77','ms184', 'cb5176', 'cc8933', 'lgc3730', 'sc11498', 'xc3007', 'mcc816', 'jed9632', 'gcd2056', 'jue203', 'mje9832', 'jg8284', 'ph2551', 'ak12124', 'eok4905', 'jl14835', 'jl13491', 'jl15934', 'yl13031', 'mal9792', 'hl4631', 'em3163', 'akm9834', 'mam10050', 'cdp4029', 'jgp7855', 'ma8368', 'rs8422', 'as17698', 'rs8559', 'ct2507', 'rt1796', 'jw9148', 'cw4742', 'hw3779', 'kw3751', 'yw8969', 'dx2098', 'rx2151', 'yy5550', 'yy5500', 'tz2880', 'xz4231', 'nz2429', 'rz2982')
+names(id)[(skip+1):(skip+nEnrolled)] <- c('gp77','ms184', 'cb5176', 'cc8933', 'lgc3730', 'sc11498', 'xc3007', 'mcc816', 'jed9632', 'gcd2056', 'jue203', 'mje9832', 'jg8284', 'ph2551', 'ak12124', 'eok4905', 'jl14835', 'jl13491', 'jl15934', 'yl13031', 'mal9792', 'hl4631', 'em3163', 'akm9834', 'mam10050', 'cdp4029', 'jgp7855', 'ma8368', 'rs8422', 'as17698', 'rs8559', 'ct2507', 'rt1796', 'jw9148', 'cw4742', 'hw3779', 'kw3751', 'yw8969', 'dx2098', 'rx2151', 'yy5550', 'yy5500', 'tz2880', 'xz4231', 'nz2429', 'rz2982')
 
 id <<- id
 
